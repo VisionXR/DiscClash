e48514a44f5b642cbf818c975c8565036f2d48bf
VisionXR <visionxr.opc@gmail.com>
2026-02-16 16:16:17 +0530

    changes

---
+++ Assets/Scripts/Views/MainCanvas/ChooseSidePanel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ChooseSidePanel : MonoBehaviour
+{
+   
+
+    public void WhiteCoinBtnClicked()
+    {
+
+    }
+
+    public void BlackCoinBtnClicked()
+    {
+
+    }
+}
