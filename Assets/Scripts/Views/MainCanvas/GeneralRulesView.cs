48ed8f8decc005f1579832203fa8a359050899d2
VisionXR <visionxr.opc@gmail.com>
2026-07-16 13:42:10 +0530

    changes

--- Assets/Scripts/Views/MainCanvas/GeneralRulesView.cs
+++ Assets/Scripts/Views/MainCanvas/GeneralRulesView.cs
@@ -1,6 +1,6 @@
 using com.VisionXR.ModelClasses;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace com.VisionXR.Views
 {
@@ -12,14 +12,39 @@ namespace com.VisionXR.Views
         public UIDataSO uiData;
 
         [Header("Panels")]
-        
+        public List<GameObject> tabObjects;
+        public List<GameObject> tabSelectionImages;
         public string rulesState;
 
+
+
+        public void TabButtonClicked(int id)
+        {
+            AudioManager.instance.PlayButtonClickSound();
+            Reset();
+            tabObjects[id].SetActive(true);
+            tabSelectionImages[id].SetActive(true);
+
+        }
 
         public void BackButtonClicked()
         {
             AudioManager.instance.PlayButtonClickSound();
             uiData.uiManager.ChangeState(rulesState, false);
+        }
+
+        private void Reset()
+        {
+            foreach (var tab in tabObjects)
+            {
+                tab.SetActive(false);
+            }
+
+            foreach (var tab in tabSelectionImages)
+            {
+                tab.SetActive(false);
+            }
+
         }
 
     }
