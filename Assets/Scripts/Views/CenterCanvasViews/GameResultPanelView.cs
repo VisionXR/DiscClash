f1603d4f36f37ecd455c506e73f6ac1f8f1cb836
VisionXR <visionxr.opc@gmail.com>
2026-09-22 15:42:44 +0530

    Changes

--- Assets/Scripts/Views/CenterCanvasViews/GameResultPanelView.cs
+++ Assets/Scripts/Views/CenterCanvasViews/GameResultPanelView.cs
@@ -137,6 +137,7 @@ namespace com.VisionXR.Views
             gameData.ResetData();
             uiInputData.GoToHome();
             uiData.uiManager.ChangeState("SinglePlayer", false);
+            uiData.uiManager.ChangeState("Match", false);
             uiData.uiManager.ChangeState("MultiPlayer", false);
             uiData.uiManager.ChangeState("Tutorial", false);
             uiData.uiManager.GoToState(StateName.HomeState);
